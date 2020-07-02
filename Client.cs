ba81a6ce807995451a0c67581520f738ac73ba22
krachi9 <55257249+krachi9@users.noreply.github.com>
2020-07-02 09:36:06 +0200

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.RegularExpressions;
+
+namespace TCP
+{
+    public static class Client
+    {
+        private static readonly BackgroundWorker senderWorker = new BackgroundWorker();
+
+        /// <summary>
+        /// Send TCP message to given IP, Port
+        /// </summary>
+        /// <param name="iP"></param>
+        /// <param name="port"></param>
+        /// <param name="message"></param>
+        /// <exception cref="Exception"> Thrown when can not connect to next peer </exception>
+        /// <exception cref="FormatException"> Thrown when IP is in wrong format, or message is empty</exception>
+        /// <exception cref="InvalidDataException">Thrown when port number is invalid</exception>
+        public static void SendMessage(string iP, string port, string message)
+        {
+            senderWorker.DoWork += SenderWorker_DoWork;
+
+            if (message?.Length == 0)
+            {
+                throw new FormatException("Message can't be empty!");
+            }
+
+            try
+            {
+                Regex pattern = new Regex(@"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b");
+
+                if (!pattern.IsMatch(iP))
+                {
+                    throw new FormatException("Invalid format for IP");
+                }
+
+                if (!int.TryParse(port, out int portInt))
+                {
+                    throw new InvalidDataException("Invalid Port Number!");
+                }
+
+                senderWorker.RunWorkerAsync(argument: new MessageClass { MessageText = message + "|" + DateTime.Now.ToString(), MessageIP = iP, MessagePort = portInt });
+            }
+            catch (SocketException)
+            {
+                throw new Exception("Can't connect to next peer");
+            }
+        }
+
+        /// <summary>
+        ///Sending a string
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="Exception">Thrown when can not connect to next peer</exception>
+        private static void SenderWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            senderWorker.DoWork -= SenderWorker_DoWork;
+            var mc = (MessageClass)e.Argument;
+
+            try
+            {
+                var client = new TcpClient(mc.MessageIP, mc.MessagePort);
+                var clientStream = client.GetStream();
+
+                using (var streamWriter = new StreamWriter(clientStream))
+                {
+                    streamWriter.Write(mc.MessageText);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+                client.Dispose();
+                clientStream.Dispose();
+                return;
+            }
+            catch (SocketException)
+            {
+                throw new Exception("Can't connect to next peer");
+            }
+        }
+
+        /// <summary>
+        /// Send TCP Station Status message to given IP, Port
+        /// </summary>
+        /// <param name="iP"></param>
+        /// <param name="port"></param>
+        /// <param name="status"></param>
+        public static void SendStationStatus(string iP, string port, StationStatus status)
+        {
+            BackgroundWorker stationSender = new BackgroundWorker();
+
+            stationSender.DoWork += StationSender_DoWork;
+
+            if (status == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            try
+            {
+                Regex pattern = new Regex(@"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b");
+
+                if (!pattern.IsMatch(iP))
+                {
+                    throw new FormatException("Invalid format for IP");
+                }
+
+                if (!int.TryParse(port, out int portInt))
+                {
+                    throw new InvalidDataException("Invalid Port Number!");
+                }
+
+                stationSender.RunWorkerAsync(argument: new StationMessage
+                {
+                    status = status,
+                    port = port,
+                    ip = iP
+                });
+            }
+            catch (SocketException)
+            {
+                throw new Exception("Can't connect to next peer");
+            }
+        }
+
+        private static void StationSender_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var msg = (StationMessage)e.Argument;
+            string ip = msg.ip;
+            string port = msg.port;
+            StationStatus status = msg.status;
+
+            IFormatter formatter = new BinaryFormatter();
+            var client = new TcpClient(ip, int.Parse(port));
+            var clientStream = client.GetStream();
+            formatter.Serialize(clientStream, status);
+            clientStream.Dispose();
+            client.Dispose();
+            System.Threading.Thread.Sleep(500);
+        }
+
+        /// <summary>
+        /// Used for TCP_Client Message sending
+        /// </summary>
+        private class MessageClass
+        {
+            public string MessageIP { get; set; }
+            public int MessagePort { get; set; }
+            public string MessageText { get; set; }
+        }
+
+        private class StationMessage
+        {
+            internal string ip;
+            internal string port;
+            internal StationStatus status;
+        }
+
+        [Serializable]
+        public class StationStatus
+        {
+            private string ip;
+
+            public string IP
+            {
+                get
+                {
+                    return ip;
+                }
+                set
+                {
+                    Regex pattern = new Regex(@"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b");
+
+                    if (!pattern.IsMatch(value))
+                    {
+                        throw new FormatException("Invalid format for IP");
+                    }
+                    else
+                    {
+                        ip = value;
+                    }
+                }
+            }
+
+            public string Message { get; set; }
+            public string StationName { get; set; }
+
+            public override string ToString()
+            {
+                return "Station " + StationName + " at " + IP + ", message: " + Message;
+            }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TCP
+{
+    public class Server
+    {
+        #region StringCommunication
+
+        private TcpListener listener;
+        private readonly BackgroundWorker tcpListenerBackgroundWorker = new BackgroundWorker();
+
+        /// <summary>
+        /// Triggers when a message is ready in public Queue messages
+        /// </summary>
+        public event EventHandler MessageArrived;
+
+        /// <summary>
+        /// FIFO storage for the messages. Dequeue to get the message when MessageArrived is triggered
+        /// </summary>
+        public Queue<string> messages = new Queue<string>();
+
+        /// <summary>
+        /// Start listening for string TCP messages on any IP  address, on given port (default = 9001)
+        /// </summary>
+        public void StartStringServer(ushort port = 9001)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            tcpListenerBackgroundWorker.DoWork += TcpListenerBackgroundWorker_DoWork;
+            tcpListenerBackgroundWorker.RunWorkerAsync();
+        }
+
+        /// <summary>
+        /// TCP string server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="Exception"></exception>
+        private void TcpListenerBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient tcpClient = listener.AcceptTcpClient();
+
+                using (NetworkStream stream = tcpClient.GetStream())
+                {
+                    StreamReader reader = new StreamReader(stream);
+                    string message = reader.ReadToEnd();
+
+                    messages.Enqueue(message);
+
+                    MessageArrived?.Invoke(this, EventArgs.Empty);
+                    reader.Dispose();
+                }
+                tcpClient.Close();
+            }
+        }
+
+        #endregion StringCommunication
+
+        #region StationStatusCommunication
+
+        private readonly BackgroundWorker stationListenerBackGroundWorker = new BackgroundWorker();
+
+        private Queue<Client.StationStatus> stationStatuses = new Queue<Client.StationStatus>();
+
+        public Queue<Client.StationStatus> StationStatuses { get => stationStatuses; set => stationStatuses = value; }
+
+        /// <summary>
+        /// Triggers when a StationStatus is ready in public Queue messages
+        /// </summary>
+        public event EventHandler StatusArrived;
+
+        /// <summary>
+        /// Start listening for StationStatus TCP messages on any IP  address, on given port (default = 9001)
+        /// </summary>
+        /// <param name="port"></param>
+        public void StartStationStatusServer(ushort port = 9001)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            stationListenerBackGroundWorker.DoWork += StationListenerBackGroundWorker_DoWork;
+            stationListenerBackGroundWorker.RunWorkerAsync();
+        }
+
+        /// <summary>
+        /// TCP string server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="Exception"></exception>
+        private void StationListenerBackGroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient tcpClient = listener.AcceptTcpClient();
+
+                using (NetworkStream stream = tcpClient.GetStream())
+                {
+                    IFormatter formatter = new BinaryFormatter();
+                    TCP.Client.StationStatus status = (TCP.Client.StationStatus)formatter.Deserialize(stream);
+                    stream.Dispose();
+                    System.Threading.Thread.Sleep(500);
+                    StationStatuses.Enqueue(status);
+                    StatusArrived?.Invoke(this, EventArgs.Empty);
+                }
+                tcpClient.Close();
+            }
+        }
+    }
+
+    #endregion StationStatusCommunication
+}
